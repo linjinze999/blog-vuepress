b7dddc5e9cdf5ae522338802d4bff70b50365f2b
Lin Jinze <linjinze@tp-link.com.cn>
2018-09-20 18:03:36 +0800

    [learning]add layout

--- docs/.vuepress/components/LearningLayout.vue
+++ docs/.vuepress/components/LearningLayout.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>{{$site}}</div>
+  <div>
+    <div class="my-ln-page">
+      <el-card shadow="hover" v-for="page in pageList.slice((currentPage-1)*pageSize,currentPage*pageSize)" :key="page.title">
+        <h2>{{ page.title }}</h2>
+        <div slot="header" class="clearfix">
+          <span>{{ page.title }}</span>
+          <span v-if="page.time" style="float: right; padding-right: 5px">{{ page.time }}</span>
+        </div>
+        <div class="my-ln-card-content">
+          {{ page.content }}
+        </div>
+      </el-card>
+      <br/>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        background
+        :current-page="currentPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="pageTotal">
+      </el-pagination>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,13 +34,37 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.prototype.$ELEMENT = {size: 'small', zIndex: 3000}
 Vue.use(ElementUI)
 export default {
+  props: {
+    pageList: {type: Array, required: true}
+  },
   data: function () {
-    return {}
+    const pageTotal = this.pageList.length
+    return {
+      currentPage: 1,
+      pageSize: 10,
+      pageTotal
+    }
+  },
+  methods: {
+    handleSizeChange (size) {
+      this.pageSize = size
+    },
+    handleCurrentChange (currentPage) {
+      this.currentPage = currentPage
+    },
   }
 }
 </script>
 
 <style scoped>
-
+.my-ln-page {
+  max-width: 600px;
+  margin: 0 auto;
+}
+.my-ln-card-content {
+  max-height: 300px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 
 </style>
--- docs/learning/frontEnd/js/README.md
+++ docs/learning/frontEnd/js/README.md
@@ -1,3 +1,17 @@
 # Javascript
 
-js学习资料
+## 你好
+
+<LearningLayout :pageList="pageList" />
+
+<script>
+export default {
+  data () {
+    return {
+      pageList: [
+        {title: '测试', time: '11', content: '11'}
+      ]
+    }
+  }
+}
+</script>
